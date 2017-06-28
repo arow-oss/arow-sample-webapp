fc487c3486212e3530f87df6b9f9d097497711dd
(cdep)illabout <cdep.illabout@gmail.com>
2017-06-28 17:00:35 +0900

    Add jwk config options to Config module.

--- src/App/Config.hs
+++ src/App/Config.hs
@@ -2,9 +2,13 @@
 module App.Config where
 
 import Control.Monad.Logger (runStdoutLoggingT)
+import Crypto.JOSE (JWK)
+import Data.Aeson (eitherDecodeStrict')
+import Data.ByteString (ByteString)
 import Database.Persist.Postgresql
        (ConnectionString, createPostgresqlPool)
 import Database.Persist.Sql (ConnectionPool)
+import Data.Semigroup ((<>))
 import Network.Wai.Handler.Warp (Port)
 import System.ReadEnvVar (lookupEnvDef, readEnvDef)
 
@@ -12,6 +16,7 @@ import App.Environment (Environment(..))
 
 data Config = Config
   { configEnvironment :: Environment
+  , configJWK :: JWK
   , configPool :: ConnectionPool
   , configPort :: Port
   } deriving Show
@@ -25,9 +30,20 @@ configFromEnv = do
     lookupEnvDef
       "DB_URL"
       "postgres://appuser:ahah8eh9ahf23hr9aaw3r@localhost:5432/appdb"
-  createConfig env port connNum connectionString
+  jwkByteString <- lookupEnvDef "JWK" devJWKByteString
+  let eitherJwk = eitherDecodeStrict' jwkByteString
+  jwk <-
+    case eitherJwk of
+      Left err ->
+        error $
+          "got following error when trying to decode json web key: " <> show err
+      Right jwk -> pure jwk
+  createConfig env jwk port connNum connectionString
 
-createConfig :: Environment -> Port -> Int -> ConnectionString -> IO Config
-createConfig env port connNum connectionString = do
+createConfig :: Environment -> JWK -> Port -> Int -> ConnectionString -> IO Config
+createConfig env jwk port connNum connectionString = do
   pool <- runStdoutLoggingT $ createPostgresqlPool connectionString connNum
-  pure $ Config env pool port
+  pure $ Config env jwk pool port
+
+devJWKByteString :: ByteString
+devJWKByteString = "{\"k\":\"XEP7OZKVglaRnGsN_sM-BI1gP6Sz3NOOpq3UcjGSf2HtOP5vomvzITpz0d0-7dCY9Rk51Pmw9ttxE0_1JOxoHKrA_8J_kiqMjESGDKav3CqdLClOnxGUmptru-muNg4_Wej5KoiQDQk1_C1z5mfLyGWwR_KS86j6lqJXrRlwXAbgK_a0hv5lG5DCsmBIah4-UDlT_loKXhEeD1APPnRqcpMW4DQ3yeV3wA84pIqdpU59f4-u5Euv47R60Ki--g87Udky9VK8MhlMc4MW6qE6jlbH9lRcIlhvpVNqr4rR9dycG6dvvmuMSB8oz3bm63WlWlp2--y95nZUhcYFQU5Z0w\",\"kty\":\"oct\"}"
